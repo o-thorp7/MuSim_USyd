92f27a87b1b00b2cdfe31667d9d4fa6c1fd18811
Oliver Thorp <otho7246@pitzer-rw01.ten.osc.edu>
2026-09-24 15:35:53 -0400

    minor adjustment to tests

--- tests/submit_debug_mkl_threads.sh
+++ tests/submit_debug_mkl_threads.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH --time=00:05:00
+#SBATCH --account=PAS2635
 #SBATCH --mem=2G
-#SBATCH --output=tests/debug_mkl_%j.out
-. config/<your_conda_config>.sh
+#SBATCH --output=tests/output/debug_mkl_%j.out
+. config/sbatch_conda_test.sh
 source "${CONDA_BASE_PATH}/etc/profile.d/conda.sh"
 conda activate "${CONDA_ENV_NAME}"
 hostname
 lscpu | grep -E "Model name|MHz|CPU\(s\):"
-python debug_mkl_threads.py
+python tests/debug_mkl_threads.py
